f890e9ef7355810f7db94a833361896044f8fe40
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-09-19 19:59:37 +0530

    Update rumabotaa.py

--- rumabotaa.py
+++ rumabotaa.py
@@ -69,19 +69,20 @@ async def forward(bot: Client, message: Message):
 	global last_message_times, user_message_count
 	if message.text == "/sub":
 		if message.reply_to_message:
+		    print(f"Checking sub for {message.reply_to_message.from_user.id}")
 		    if user_message_count.get(message.reply_to_message.from_user.id, 0) >= int(max_posts_per_day):
 		        remaining_posts = 0
 		    else:
 		        remaining_posts = int(max_posts_per_day) - user_message_count.get(message.reply_to_message.from_user.id, 0)
 		        remaining_posts_message = f"• Remaining Post :{remaining_posts} out of {max_posts_per_day} posts today\n\n• Total Posted = {user_message_count.get(message.reply_to_message.from_user.id, 0)}"
-		        return await message.reply_text(remaining_posts_message)
+		    return await message.reply_text(remaining_posts_message)
 		else:
 		    if user_message_count.get(user_id, 0) >= int(max_posts_per_day):
 		        remaining_posts = 0
 		    else:
 		        remaining_posts = int(max_posts_per_day) - user_message_count.get(user_id, 0)
 		        remaining_posts_message = f"• Remaining Post :{remaining_posts} out of {max_posts_per_day} posts today\n\n• Total Posted = {user_message_count.get(user_id, 0)}"
-		        return await message.reply_text(remaining_posts_message)
+		    return await message.reply_text(remaining_posts_message)
 	try:
 	    if message.text.startswith('.') or message.text.startswith('/'):
 		    return print("Message Started From . or /")
