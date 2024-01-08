c23c72e93255caea443e50131774524c6e1e82b8
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-08 11:34:36 +0530

    Update rumabotaa.py

--- rumabotaa.py
+++ rumabotaa.py
@@ -9,11 +9,11 @@ from logging.handlers import RotatingFileHandler
 
 api_id = 3748059
 api_hash = "f8c9df448f3ba20a900bc2ffc8dae9d5"
-bot_token = "6961656001:AAHvhsXsKopXJRfmZAoVsfvmtNPbEmauh6w"
-chat_id = -1002038780063
-channel_id = -1001930768149
-mp_link = "https://t.me/FrigidMarket"
-allow_id = "6162272468"
+bot_token = "6320403496:AAEK8iBPhDettDzW34dY8GBLEc98JaCXI-Q"
+chat_id = -1001986181510
+channel_id = -1001964061984
+mp_link = "https://t.me/+oVqB_bTso2VjNmU9"
+allow_id = "1155668831"
 max_posts_per_day = 4
 max_time = 300
 
