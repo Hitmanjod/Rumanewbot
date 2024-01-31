ab5ed458d7975c0c6f2535db80a8b171c710d2df
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-31 23:36:55 +0530

    Update rumabotaa.py

--- rumabotaa.py
+++ rumabotaa.py
@@ -9,13 +9,13 @@ from logging.handlers import RotatingFileHandler
 
 api_id = 3748059
 api_hash = "f8c9df448f3ba20a900bc2ffc8dae9d5"
-bot_token = "6824427158:AAGY_bc3ewOVkqc0kntTYm3V3yCLotXx4Ao"
-chat_id = -1002083154176
-channel_id = -1001684367226
-mp_link = "https://t.me/+WJC6DmGc62E2ODM0"
-allow_id = "1816863256"
-max_posts_per_day = 3
-max_time = 600
+bot_token = "6648894114:AAF-Lv3IalLBVq0DJ8YUXJglUCNFY671ZI4"
+chat_id = -1001610151919
+channel_id = -1001903796053
+mp_link = "https://t.me/shelbympbot"
+allow_id = "5475778989"
+max_posts_per_day = 4
+max_time = 300
 
 logging.basicConfig(
     level=logging.DEBUG,
