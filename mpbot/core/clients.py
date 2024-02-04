baa25d5cc75265baf1f89e84bd1e45464abc523a
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-02-04 14:04:42 +0530

    Create clients.py

---
+++ mpbot/core/clients.py
@@ -0,0 +1,32 @@
+import asyncio
+import importlib
+from pyromod import listen
+from pyrogram import Client, idle
+from pyrogram.errors import FloodWait
+
+from mpbot.Config import API_HASH, API_ID, BOT_TOKEN
+from mpbot.plugins import ALL_MODULES
+
+from .logger import LOGS
+
+async def Start_MPBot():
+    try:
+        app = Client(
+            "app",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins=dict(root="mpbot/plugins"),
+          )
+        await app.start()
+    except FloodWait as e:
+          LOGS.error(f"Bot Wants to Sleep For {e.value}")
+          await asyncio.sleep(e.value + 5)
+    except Exception as f:
+          LOGS.error(f)
+    for all_module in ALL_MODULES:
+          importlib.import_module("mpbot.plugins." + all_module)
+          LOGS.info(f"➢ Successfully Imported : {all_module}")
+    LOGS.info("==============================")
+    LOGS.info("==============================")
+    await idle()
