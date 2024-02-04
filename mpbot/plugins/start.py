6197c94f1256c6000be5d7db2d7d3520df311875
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-02-04 10:16:35 +0000

    pylint: auto fixes

--- mpbot/plugins/start.py
+++ mpbot/plugins/start.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from mpbot.Config import MP_LINK
 
