099d64a53f871df644fa470050f9df760869dda1
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-02-05 12:23:06 +0000

    pylint: auto fixes

--- mpbot/plugins/reedem.py
+++ mpbot/plugins/reedem.py
@@ -89,9 +89,10 @@ async def reeyydemf(bot, message):
         expire_time = (datetime.now() + timedelta(days=7)).strftime("%Y-%m-%d %H:%M:%S")
         if user_id in ok:
             days_left = (
-                datetime.strptime(ok[user_id], "%Y-%m-%d %H:%M:%S") - datetime.now()) 
-            + datetime.now() 
-            + timedelta(days=7).strftime("%Y-%m-%d %H:%M:%S")
+                datetime.strptime(ok[user_id], "%Y-%m-%d %H:%M:%S") - datetime.now()
+            )
+            +datetime.now()
+            +timedelta(days=7).strftime("%Y-%m-%d %H:%M:%S")
             add_expiration(user_id, days_left)
             return await message.reply(f"Plan extended till {days_left}")
         add_expiration(user_id, str(expire_time))
