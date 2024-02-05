89082a2ce7482101650a4e7eae86f81b81065bac
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-02-05 19:15:19 +0530

    Update reedem_db.py

--- mpbot/database/reedem_db.py
+++ mpbot/database/reedem_db.py
@@ -11,7 +11,11 @@ def add_seven_code(code):
         ok.append(code)
         return legend_db.set_key("SCODE", ok)
 
-
+def remove_seven_code(code):
+    ok = get_seven_code()
+    ok.remove(code)
+    return legend_db.set_key("SCODE", ok)
+    
 def get_monthly_code():
     return legend_db.get_key("MCODE") or []
 
@@ -21,6 +25,11 @@ def add_monthly_code(code):
     if not code in ok:
         ok.append(code)
         return legend_db.set_key("MCODE", ok)
+
+def remove_monthly_code(code):
+    ok = get_monthly_code()
+    ok.remove(code)
+    return legend_db.set_key("MCODE", ok)
 
 
 def user_expiration():
