5e612e6566527e319c627953139a874e29366ee8
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-02-05 20:23:00 +0530

    Update clients.py

--- mpbot/core/clients.py
+++ mpbot/core/clients.py
@@ -9,6 +9,7 @@ from pyrogram.errors import FloodWait
 from mpbot.Config import API_HASH, API_ID, BOT_TOKEN
 from mpbot.plugins import ALL_MODULES
 
+from ..database.all_db import legend_db
 from ..database.reedem_db import *
 from .logger import LOGS
 
@@ -24,20 +25,28 @@ app = Client(
 )
 
 
-def check_member():
+async def check_member():
     ok = user_expiration()
     for user_id, expire in ok.items():
+        renew_days = (datetime.strptime(ok[user_id], "%Y-%m-%d %H:%M:%S") - datetime.now()).days
+        if 2 < renew_days < 3 or 1 > renew_days > 0:
+            try:
+                await app.send_message(user_id, f"Your subscription is ending in {renew_days} days. Buy a new key if you want to keep using this bot.")
+            except:
+                pass
         expire_time = datetime.strptime(ok[user_id], "%Y-%m-%d %H:%M:%S")
-        # king = expire_time
-        print(type(expire_time))
         if datetime.now() > expire_time:
+            await app.kick_chat_member(CHAT_ID, user_id)
             ok.pop(user_id)
+    legend_db.set_key("EXPIRATION", ok)
+    
 
 
 async def Start_MPBot():
     try:
-        check_member()
         await app.start()
+        await check_member()
+        await app.send_message(CHAT_ID, f"Hello")
     except FloodWait as e:
         LOGS.error(f"Bot Wants to Sleep For {e.value}")
         await asyncio.sleep(e.value + 5)
