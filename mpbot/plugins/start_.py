170014b5cd830e59b17c00576b864559342aef7f
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-02-05 21:34:24 +0530

    Update start_.py

--- mpbot/plugins/start_.py
+++ mpbot/plugins/start_.py
@@ -29,17 +29,34 @@ async def staaart(bot, msg):
 
 @app.on_callback_query(filters.regex("stats"))
 async def stats(_, query: CallbackQuery):
-    user_id = "Stats Of MarketPlace"
+    await query.edit_message_text("Processing...")
+    user_id = "Stats Of MarketPlace\n\n"
+    ok = user_expiration()
+    for user_id, expire in ok.items():
+        peer_idsolve = await app.resolve_peer(user_id)
+        user = await bot.get_users(peer_idsolve)
+        name = user.first_name
+        user_id += f"• {name} : {expire}")
     await query.edit_message_text(user_id)
 
 
 @app.on_callback_query(filters.regex("developer"))
-async def stats(_, query: CallbackQuery):
-    user_id = "Developer"
+async def dveke(_, query: CallbackQuery):
+    user_id = """
+    I am a skilled Coder, serving over 100 clients monthly with swift turnaround times. Specializing in Custom Bot creation, I exclusively accept cryptocurrency payments.
+    **Developer** : [Legend](https://t.me/LegendBoy_OP)
+    """
     await query.edit_message_text(user_id)
 
 
 @app.on_callback_query(filters.regex("about"))
-async def stats(_, query: CallbackQuery):
-    user_id = "About"
+async def abouts(_, query: CallbackQuery):
+    user_id = """
+Description : This telegram bot can be used for your marketplace or any other work.
+✔️Functions : By this bot you can add special users in a private group and everything which they will post inside the private group will be forwarded inside the linked channel.
+✔️ Commands :
+/sub : Check Posts Left / day.
+/redeem : Redeem key to get post bot access.
+/start : Check whether bot is alive or not.
+    """
     await query.edit_message_text(user_id)
