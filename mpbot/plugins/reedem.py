c29bd2d7af4958d61d6385d559ace4d68d58e22c
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-02-05 18:01:09 +0530

    Update reedem.py

--- mpbot/plugins/reedem.py
+++ mpbot/plugins/reedem.py
@@ -92,7 +92,7 @@ async def reeyydemf(bot, message):
                 datetime.strptime(ok[user_id], "%Y-%m-%d %H:%M:%S") - datetime.now()
             )
             +datetime.now()
-            +timedelta(days=7).strftime("%Y-%m-%d %H:%M:%S")
+            +timedelta(days=7)
             add_expiration(user_id, days_left)
             return await message.reply(f"Plan extended till {days_left}")
         add_expiration(user_id, str(expire_time))
@@ -142,5 +142,5 @@ async def subhh(bot, msg):
         return await msg.reply_text(
             "You didnt subscribed my bot Contact owner to subscribe my bot"
         )
-    days_left = (ok[user_id] - datetime.now()).days
+    days_left = datetime.strptime(ok[user_id], "%Y-%m-%d %H:%M:%S") - datetime.now()
     await msg.reply_text(f"Days left for your access - {days_left}")
